fb9d7e13d2ada9d1c1b5a2bbe40e517a7177617c
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-02-15 23:08:00 +0300

    refactor: change children width

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -4,10 +4,10 @@ import Navigation from "@/app/dashboard/_components/navigation";
 function DashboardLayout({children} :PropsWithChildren) {
     return (
         <main className='bg-white flex h-screen'>
-            <aside className='flex flex-[7%] flex-col bg-[#EAD9CC]'>
+            <aside className='flex flex-[5%] flex-col bg-[#EAD9CC]'>
                 <Navigation/>
             </aside>
-            <div className='flex flex-[90%]'>
+            <div className='flex flex-[95%]'>
                 {children}
             </div>
         </main>
