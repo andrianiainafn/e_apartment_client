676e4224023a4a1c68b361910210814683eab633
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-02-25 00:54:05 +0300

    feature : add edit button

--- src/app/dashboard/_components/button/button_edit.tsx
+++ src/app/dashboard/_components/button/button_edit.tsx
@@ -1,9 +1,30 @@
+'use client'
 import React from 'react';
 import { MdOutlineEdit } from "react-icons/md";
+import {useFetchApartmentDetails} from "@/app/dashboard/apartment/_hooks/use_fetch_apartment";
+import {APARTMENT_ENDPOINT} from "@/app/dashboard/apartment/_constant/endpoint";
+import {useApartmentDetails} from "@/state/global_state";
 
-const ButtonEdit = () => {
+interface Props{
+    HandleClickEdit:()=>void
+    numApp:string
+}
+
+const ButtonEdit = (props:Props) => {
+    const {isSuccess,data} = useFetchApartmentDetails(props.numApp)
+    const updateDesign = useApartmentDetails((state)=>state.updateDesign)
+    const updateNumApp = useApartmentDetails((state)=>state.updateNumApp)
+    const updateRent = useApartmentDetails((state)=>state.updateRent)
+    const HandleClickEdit  = ()=>{
+        if(isSuccess){
+            updateDesign(data.data.design)
+            updateRent(data.data.rent)
+            updateNumApp(props.numApp)
+            props.HandleClickEdit()
+        }
+    }
     return (
-        <button className=' border border-[#D3A286] text-[#D3A286] text-sm rounded-xl h-[4vh] w-[6vw] justify-center flex space-x-1 items-center' >
+        <button onClick={HandleClickEdit} className=' border border-[#D3A286] text-[#D3A286] text-sm rounded-xl h-[4vh] w-[6vw] justify-center flex space-x-1 items-center' >
             <p>Edit</p>
             <MdOutlineEdit className='text-xl'/>
         </button>
