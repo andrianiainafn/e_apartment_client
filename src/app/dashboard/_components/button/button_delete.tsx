f15e429d91d117632b1c2610471e0ba3ca1dada0
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-02-26 19:30:47 +0300

    feature: add click action

--- src/app/dashboard/_components/button/button_delete.tsx
+++ src/app/dashboard/_components/button/button_delete.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { MdDeleteOutline } from "react-icons/md";
 
-const ButtonDelete = () => {
+interface Props{
+    HandleClick: ()=>void
+}
+const ButtonDelete = (props:Props) => {
     return (
-        <button className=' border border-red-600 text-red-600 text-sm rounded-xl h-[4vh] w-[6vw] justify-center  flex space-x-1 items-center' >
+        <button onClick={props.HandleClick} className=' border border-red-600 text-red-600 text-sm rounded-xl h-[4vh] w-[6vw] justify-center  flex space-x-1 items-center' >
             <p>Delete</p>
             <MdDeleteOutline className='text-red-600 text-xl'/>
         </button>
