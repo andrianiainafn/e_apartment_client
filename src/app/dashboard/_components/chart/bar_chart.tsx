5a75cd00a88933c60e8bb3979d1907fc88d79e35
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-02-26 19:31:21 +0300

    feature: make chart dynamic

--- src/app/dashboard/_components/chart/bar_chart.tsx
+++ src/app/dashboard/_components/chart/bar_chart.tsx
@@ -1,48 +1,45 @@
 'use client'
 import { BarChart } from '@tremor/react';
+import {useFetchApartmentStat} from "@/app/dashboard/apartment/_hooks/use_fetch_apartment";
+import {useEffect, useState} from "react";
 
-const chartdata = [
-    {
-        name: 'Amphibians',
-        'Number of threatened species': 2488,
-    },
-    {
-        name: 'Birds',
-        'Number of threatened species': 1445,
-    },
-    {
-        name: 'Crustaceans',
-        'Number of threatened species': 743,
-    },
-    {
-        name: 'Ferns',
-        'Number of threatened species': 281,
-    },
-    {
-        name: 'Arachnids',
-        'Number of threatened species': 251,
-    },
-    {
-        name: 'Corals',
-        'Number of threatened species': 232,
-    },
-    {
-        name: 'Algae',
-        'Number of threatened species': 98,
-    },
-];
 
 const dataFormatter = (number: number) =>
     Intl.NumberFormat('us').format(number).toString();
-
-export const BarChartHero = () => (
-    <BarChart
-        data={chartdata}
-        index="name"
-        categories={['Number of threatened species']}
-        colors={['red']}
-        valueFormatter={dataFormatter}
-        yAxisWidth={48}
-        onValueChange={(v) => console.log(v)}
-    />
-);
+interface IData {
+    name:string
+    "Number of rent": number
+}
+export const BarChartHero = () => {
+    const {isLoading,isSuccess,data} = useFetchApartmentStat()
+    const [dataChart,setDataChart] = useState<IData[]>([])
+    useEffect(()=>{
+        if (isSuccess){
+            console.log(data.data)
+            setDataChart(prevState => [...prevState,
+                {
+                    name:"minimal",
+                    "Number of rent" :data.data.min
+                },
+                {
+                    name:"maximal",
+                    "Number of rent" :data.data.max
+                },
+                {
+                    name:"total",
+                    "Number of rent" :data.data.total
+                },
+            ])
+        }
+    },[isSuccess])
+    return (
+        <BarChart
+            data={dataChart}
+            index="name"
+            categories={['Number of rent']}
+            colors={['red']}
+            yAxisWidth={48}
+            onValueChange={(v) => console.log(v)}
+        />
+    )
+}
